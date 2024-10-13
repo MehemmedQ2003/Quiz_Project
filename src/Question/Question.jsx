2c60e54b30fc4219e8f0af1bfa734cc3ed6bcf7d
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2024-10-13 13:03:42 +0400

    Quiz app downloaded

--- src/Question/Question.jsx
+++ src/Question/Question.jsx
@@ -7,6 +7,7 @@ const Question = ({ questions }) => {
   const [userAnswers, setUserAnswers] = useState({});
   const [showResults, setShowResults] = useState(false);
   const [questionsAnswered, setQuestionsAnswered] = useState(0);
+  const [showAlert, setShowAlert] = useState(false); // Xəbərdarlıq üçün state
 
   const handleAnswerSelect = (questionId, answer) => {
     setUserAnswers(prevAnswers => ({
@@ -17,7 +18,12 @@ const Question = ({ questions }) => {
   };
 
   const handleSubmit = () => {
-    setShowResults(true);
+    if (questionsAnswered < questions.length) {
+      setShowAlert(true); // Xəbərdarlığı göstər
+    } else {
+      setShowResults(true);
+      setShowAlert(false); // Xəbərdarlığı gizlət
+    }
   };
 
   const getAnswerStatus = (question, userAnswer) => {
@@ -58,25 +64,32 @@ const Question = ({ questions }) => {
           )}
         </div>
       ))}
-      {questionsAnswered === 0 && !showResults && (
+      {!showResults && (
         <button onClick={handleSubmit} className="submit-button btn btn-primary">
-          Submit Answers
+          Cavabları götür
         </button>
+      )}
+      {showAlert && (
+        <div className="alert alert-danger bg-danger text-white alert-dismissible fade show my-4" role="alert">
+          Zəhmət olmasa bütün sualları cavablandırın.
+          <button type="button" className="btn-close" onClick={() => setShowAlert(false)} aria-label="Close"></button>
+        </div>
       )}
       {showResults && (
         <div className="results">
           <h2 className='text-center fw-bold bg-success text-white p-3 rounded-3 mt-5 mb-3'>Results</h2>
-            {questions.map((question) => (
-              <div key={question.id} className="result-item p-3  border border-primary rounded-3 my-3 w-75 mx-auto">
-                <p><b style={{color: 'red', fontSize: '20px'}}>{question.id}.</b> Sual: {question.questionTitle}</p>
-                <p><b>Sənin cavabın:</b> {userAnswers[question.id] || 'Not answered'}</p>
-                <p><b>Doğru cavab:</b> {question.correctAnswers.join(', ')}</p>
-              </div>
-            ))}
+          {questions.map((question) => (
+            <div key={question.id} className="result-item p-3 border border-primary rounded-3 my-3 w-75 mx-auto">
+              <p><b style={{color: 'red', fontSize: '20px'}}>{question.id}.</b> Sual: {question.questionTitle}</p>
+              <p><b>Sənin cavabın:</b> {userAnswers[question.id] || 'Not answered'}</p>
+              <p><b>Doğru cavab:</b> {question.correctAnswers.join(', ')}</p>
+            </div>
+          ))}
         </div>
       )}
     </div>
   );
 };
+
 
 export default Question;
